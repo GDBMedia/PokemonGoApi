8b5a5517d0eab0cde3ef62521b8e0c13051284c9
Garrett Biernat <garrett@gdbmedia.net>
2016-07-29 18:06:49 -0700

    added port

--- pmgWebApi.js
+++ pmgWebApi.js
@@ -64,9 +64,9 @@ app.get('/getuserinfo/', function (req, res) {
                 }
               });
 });
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
 
 function loopResponseUser(responses, responsesUser) {
   var userdata = {
